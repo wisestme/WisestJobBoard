334f08897b18840ac26e67a2431c74ce58e77960
wisestMe <wyhzest@gmail.com>
2020-11-14 03:31:17 +0100

    Add project files.

---
+++ JobberGuys/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobberGuys.Startup))]
+namespace JobberGuys
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
