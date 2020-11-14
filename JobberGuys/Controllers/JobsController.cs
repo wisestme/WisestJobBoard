27f74d320ca6042d0ebb9346e0c67398c1ce4984
wisestMe <wyhzest@gmail.com>
2020-11-14 04:01:59 +0100

    add job

---
+++ JobberGuys/Controllers/JobsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JobberGuys.Models;
+
+namespace JobberGuys.Controllers
+{
+    public class JobsController : Controller
+    {
+        // GET: Jobs
+        public ActionResult Jobs()
+        {
+            var job = new Job() { Title = "Web Developer" };
+            return View(job);
+        }
+    }
+}
