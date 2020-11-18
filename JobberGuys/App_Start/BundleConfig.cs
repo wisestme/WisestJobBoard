67408a243381d521410a01e576854b564b4a891c
wisestMe <wyhzest@gmail.com>
2020-11-18 08:36:25 +0100

    add fontawesome

--- JobberGuys/App_Start/BundleConfig.cs
+++ JobberGuys/App_Start/BundleConfig.cs
@@ -24,7 +24,10 @@ namespace JobberGuys
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/fontawesome.css"));
+            
         }
+        
     }
 }
